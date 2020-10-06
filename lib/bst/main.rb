0cabdd5120c4acf4d9d18f78e8ee4d73b58429f6
Zach Baird <zdb1994@yahoo.com>
2020-10-05 22:50:48 -0400

    Added insertion and deletion class methods.

--- lib/bst/main.rb
+++ lib/bst/main.rb
@@ -3,3 +3,17 @@ require_relative 'tree'
 my_tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
 my_tree.traverse(my_tree.root)
+
+my_tree.insert(121)
+
+my_tree.traverse(my_tree.root)
+
+one_node = my_tree.find(4)
+puts one_node
+puts one_node.value
+
+my_tree.pretty_print
+
+my_tree.delete(121)
+
+my_tree.pretty_print
--- lib/bst/tree.rb
+++ lib/bst/tree.rb
@@ -4,13 +4,14 @@ require_relative 'node'
 
 # A balanced binary search tree
 class Tree
-  attr_accessor :root
+  attr_reader :root
 
   def initialize(arr)
-    @root = build_tree(arr)
+    @root = build_tree arr
   end
 
   def build_tree(arr)
+    arr = arr.uniq
     root = Node.new(arr.shift)
 
     arr.each { |x| push_node(root, x) }
@@ -18,27 +19,96 @@ class Tree
     root
   end
 
-  def traverse(node)
-    return if @root.nil? || node.nil?
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_leaf, "#{prefix}#{is_left ? '│ ' : ' '}", false) if node.right_leaf
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left_leaf, "#{prefix}#{is_left ? ' ' : '│ '}", true) if node.left_leaf
+  end
 
-    traverse(node.left_leaf) if node.left_leaf
+  def traverse(node = @root)
+    return if node.nil?
+
+    traverse node.left_leaf if node.left_leaf
+
     puts node.value
-    traverse(node.right_leaf) if node.right_leaf
+
+    traverse node.right_leaf if node.right_leaf
   end
 
   def insert(value)
-    insert_traversal(@root, value)
+    insert_traversal @root, value
+  end
+
+  def delete(value, node = @root)
+    if node.value == value
+      if one_leaf?(node)
+        @root = node.left_leaf || node.right_leaf
+      else
+        next_right_num = node.right_leaf
+        second_smallest = next_right_num
+
+        second_smallest = second_smallest.left_leaf while second_smallest.left_leaf.left_leaf
+
+        node.value = second_smallest.left_leaf.value
+        second_smallest.left_leaf = second_smallest.left_leaf.right_leaf
+      end
+    elsif node.left_leaf && node.left_leaf.value == value
+      if leaf?(node.left_leaf)
+        node.left_leaf = nil
+      elsif one_child?(node.left_leaf)
+        node.left_leaf = node.left_leaf.left_leaf || node.left_leaf.right_leaf
+      else
+        next_right_num = node.left_leaf.right_leaf
+        second_smallest = next_right_num
+
+        second_smallest = second_smallest.left_leaf while second_smallest.left_leaf.left_leaf
+
+        node.left_leaf.value = second_smallest.left_leaf.value
+        second_smallest.left_leaf = second_smallest.left_leaf.right
+      end
+    elsif node.right_leaf && node.right_leaf == value
+      if leaf?(node.right_leaf)
+        node.right_leaf = nil
+      elsif one_child?(node.right_leaf)
+        node.right_leaf = node.right_leaf.left_leaf || node.right_leaf.right_leaf
+      else
+        next_right_num = node.left_leaf.right_leaf
+        second_smallest = next_right_num
+
+        second_smallest = second_smallest.left_leap while second_smallest.left_leaf.left_leaf
+
+        node.right_leaf.value = second_smallest.left_leaf.value
+        second_smallest.left_leaf = second_smallest.left_leaf.right_leaf
+      end
+    else
+      if value < node.value
+        delete value, node.left_leaf
+      else
+        delete value, node.right_leaf
+      end
+    end
+  end
+
+  def find(value, node = @root)
+    return if node.nil?
+    return node if node.value == value
+
+    if value < node.value
+      find value, node.left_leaf
+    else
+      find value, node.right_leaf
+    end
   end
 
   private
 
   def insert_traversal(node, value)
     if value < node.value && node.left_leaf
-      insert_traversal(node.left_leaf)
+      insert_traversal(node.left_leaf, value)
     elsif value < node.value && !node.left_leaf
       node.left_leaf = Node.new(value)
     elsif value > node.value && node.right_leaf
-      insert_traversal(node.right_leaf)
+      insert_traversal(node.right_leaf, value)
     elsif value > node.value && !node.right_leaf
       node.right_leaf = Node.new(value)
     end
@@ -54,5 +124,14 @@ class Tree
 
       node.left_leaf = Node.new(value)
     end
+  end
+
+  def leaf?(node)
+    node.left_leaf.nil? && node.right_leaf.nil?
+  end
+
+  def one_leaf?(node)
+    (node.left_leaf && node.right_leaf.nil?) ||
+      (node.left_leaf.nil? && node.right_leaf)
   end
 end
